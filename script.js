80a43f22dcd04b39a3b0299791339ec3ee940b4d
Tun Lin <lintun2003@gmail.com>
2024-05-23 17:06:04 -0400

    created adjustable grid and added button that can change amount of div

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
 const container = document.getElementById("container")
 
-for (let i = 0; i < 16 * 16; i++){
-    const div = document.createElement("div");
-    div.classList.add('grid');
+const button = document.querySelector("button")
+button.addEventListener("click", () => {
+    removeChildren();
+    const size = prompt("enter number of grid")
+    generateGrid(size);
 
-    div.textContent = i + 1;
-    container.appendChild(div)
+})
 
+function generateGrid (size){
+    for (let i = 0; i < size * size; i++){
+        const div = document.createElement("div");
+        div.classList.add('grid');
+        container.appendChild(div);
+        div.style.width = `calc(100% / ${size})`;
+        div.style.height = `calc(100% / ${size})`
+
+    }
+    const div = document.querySelectorAll("div")
+    div.forEach(div => {
+        div.addEventListener("mouseenter", () => {
+        div.style.backgroundColor = "black";
+        })
+        
+    });
+
+}
+
+function removeChildren(){
+    while (container.firstChild){
+        container.removeChild(container.firstChild)
+    }
 }
